5448f8332885de7866f29fea4a9c8e2c37c2788f
daWinzig <42668927+daWinzig@users.noreply.github.com>
2022-07-01 12:47:50 +0200

    Added ActionBar text on toggle on/off

--- src/main/java/net/dawinzig/goatseparator/GoatSeparator.java
+++ src/main/java/net/dawinzig/goatseparator/GoatSeparator.java
@@ -1,29 +1,39 @@
 package net.dawinzig.goatseparator;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.option.StickyKeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class GoatSeparator implements ClientModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
+	@SuppressWarnings("unused")
 	public static final Logger LOGGER = LoggerFactory.getLogger("goatseparator");
 
 	public static KeyBinding keyBinding;
+	private static boolean wasPressed = false;
 	@Override
 	public void onInitializeClient() {
 		keyBinding = KeyBindingHelper.registerKeyBinding(new StickyKeyBinding(
-				"key.goatseparator.show", // The translation key of the keybinding's name
-				GLFW.GLFW_KEY_LEFT_ALT, // The keycode of the key
-				"category.goatseparator.main", // The translation key of the keybinding's category.
+				"key.goatseparator.show",
+				GLFW.GLFW_KEY_LEFT_ALT,
+				"category.goatseparator.main",
 				() -> true
 		));
+		ClientTickEvents.END_CLIENT_TICK.register(client -> {
+			if (keyBinding.isPressed() && !wasPressed) {
+				assert client.player != null; // should never happen
+				client.player.sendMessage(Text.translatable("text.goatseparator.toggled.on"), true);
+				wasPressed = keyBinding.isPressed();
+			} else if (!keyBinding.isPressed() && wasPressed) {
+				assert client.player != null; // should never happen
+				client.player.sendMessage(Text.translatable("text.goatseparator.toggled.off"), true);
+				wasPressed = keyBinding.isPressed();
+			}
+		});
 	}
 }
